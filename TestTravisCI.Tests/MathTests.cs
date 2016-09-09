fc8214433a83e4b89ee3a42fdd68a861b3b71040
Frank DeCaire <frank@decaire.net>
2016-09-08 21:06:27 -0400

    Creating Git Repo

---
+++ TestTravisCI.Tests/MathTests.cs
@@ -0,0 +1,28 @@
+﻿using Xunit;
+
+namespace TestTravisCI.Tests
+{
+	public class MathTests
+	{
+		[Fact]
+		public void PassingTest()
+		{
+			Assert.Equal(5, 5);
+		}
+
+		[Fact]
+		public void PassingTest2()
+		{
+			Assert.True(true);
+		}
+
+		[Theory]
+		[InlineData(3)]
+		[InlineData(5)]
+		[InlineData(7)]
+		public void TestIsOdd(int value)
+		{
+			Assert.True(MyMathFunctions.IsOdd(value));
+		}
+	}
+}
---
+++ TestTravisCI/MyMathFunctions.cs
@@ -0,0 +1,10 @@
+﻿namespace TestTravisCI
+{
+    public class MyMathFunctions
+    {
+		public static bool IsOdd(int value)
+		{
+			return value % 2 == 1;
+		}
+	}
+}
