3eb9f8737569a3add20400ca67462328f6854d77
Frank DeCaire <frank@decaire.net>
2016-09-08 21:15:26 -0400

    Created add() method

--- TestTravisCI.Tests/MathTests.cs
+++ TestTravisCI.Tests/MathTests.cs
@@ -24,5 +24,13 @@ namespace TestTravisCI.Tests
 		{
 			Assert.True(MyMathFunctions.IsOdd(value));
 		}
+
+		[Theory]
+		[InlineData(2, 3)]
+		[InlineData(23,12)]
+		public void TestAdd(double val1, double val2)
+		{
+			Assert.Equal(val1 + val2, MyMathFunctions.Add(val1, val2));
+		}
 	}
 }
--- TestTravisCI/MyMathFunctions.cs
+++ TestTravisCI/MyMathFunctions.cs
@@ -1,10 +1,15 @@
 ﻿namespace TestTravisCI
 {
-    public class MyMathFunctions
+    public static class MyMathFunctions
     {
 		public static bool IsOdd(int value)
 		{
 			return value % 2 == 1;
+		}
+
+		public static double Add(this double num1, double num2)
+		{
+			return num1 + num2;
 		}
 	}
 }
